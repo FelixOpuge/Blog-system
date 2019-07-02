aac531b44f60a115e62d3ac6b76d941ab8bc86a5
OpugeF <omondifelix702@gmail.com>
2019-07-02 12:55:51 +0300

    adding

--- users/views.py
+++ users/views.py
@@ -9,7 +9,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account as been created! you are now able to log in')
+            messages.success(request, 'Your account as been created! you are now able to log in')
             return redirect('login')
     else:
         form =UserRegisterForm()
@@ -24,7 +24,7 @@ def profile(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Your account as been updated')
+            messages.success(request, 'Your account as been updated')
             return redirect('profile')
     else:
         u_form = UserUpdateForm(instance=request.user)
